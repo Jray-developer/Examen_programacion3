dac804b4dd5ed4cdfbfc58e25cc6b69715766938
Jray-developer <jlraymundo03@gmail.com>
2026-04-18 17:23:11 -0600

    Implementación de CRUD de productos y corrección de pipeline

---
+++ tests/test_products.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.database.db_mysql import Base, get_db
+from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+
+def test_read_main():
+    Base.metadata.create_all(bind=engine)
+    response = client.get("/")
+    assert response.status_code == 200
